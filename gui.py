b2c7d405ce3904507997dead29c1b694fd92d497
brandenkaestner <brandenk514@gmail.com>
2018-04-29 22:49:27 -0400

    updated gui

--- gui.py
+++ gui.py
@@ -1,47 +1,51 @@
 import wx
-import os
 
 
-class GUI(wx.Frame):
-
+class MainFrame(wx.Frame):
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(400, 600))
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.CreateStatusBar()
+        wx.Frame.__init__(self, parent, -1, title=title)
 
-        file_menu = wx.Menu()
-        # wx.ID_ABOUT and wx.ID_EXIT are standard ids provided by wxWidgets.
+        # Setting up the menu.
+        filemenu = wx.Menu()
 
-        menuAbout = file_menu.Append(wx.ID_FILE, "&About", " Information about this program")
+        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
+        filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT, "&Exit", " Terminate the program")
 
-        menuExit = file_menu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
-        menuFile = file_menu.Append(wx.ID_FILE, "&Test", "File button")
         # Creating the menubar.
         menuBar = wx.MenuBar()
-        menuBar.Append(file_menu, "&File")  # Adding the "file_menu" to the MenuBar
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
         self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
-        # Set events.
-        self.Bind(wx.EVT_MENU, self.OnFile, menuFile)
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         self.Show(True)
 
-    def OnFile(self, e):
-        dlg = wx.MessageDialog(self, "Test", "About Test", wx.OK)
-        dlg.ShowModal()  # Show it
-        dlg.Destroy()
 
-    def OnAbout(self, e):
-        # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
-        dlg = wx.MessageDialog(self, "A small text editor", "About Sample Editor", wx.OK)
-        dlg.ShowModal()  # Show it
-        dlg.Destroy()  # finally destroy it when finished.
+class MainPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, -1, size=(300, 300))
 
-    def OnExit(self, e):
-        self.Close(True)  # Close the frame.
+        self.grid_sizer = wx.GridBagSizer(hgap=3, vgap=3)
+
+        self.weather_text = wx.StaticText(self, label="Here is some weather")
+        self.grid_sizer.Add(self.weather_text, pos=(0, 0))
+
+        self.time_text = wx.StaticText(self, label="12:00")
+        self.grid_sizer.Add(self.time_text, pos=(0, 3))
+
+        # A button
+        self.button = wx.Button(self, label="Ask")
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
+        self.grid_sizer.Add(self.button, pos=(2, 2))
+
+        self.SetSizerAndFit(self.grid_sizer)
+
+    def OnClick(self):
+        s = 0
 
 
-app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
-frame = GUI(None, "Test Frame")
-frame.Show(True)  # Show the frame.
-app.MainLoop()
+if __name__ == '__main__':
+    app = wx.App(False)
+    frame = MainFrame(None, "HAL")
+    panel = MainPanel(frame)
+    frame.Show()
+    app.MainLoop()
