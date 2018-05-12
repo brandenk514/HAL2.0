44b8802fe4392cf164ab6eaa5ead23bc1bdc083a
brandenkaestner <brandenk514@gmail.com>
2018-05-12 13:37:47 -0400

    changed scope

--- hal.py
+++
@@ -1,9 +0,0 @@
-import motion_sensor
-
-class Hal:
-    def __init__(self):
-        self.motion_sensor = motion_sensor.MotionSensor()
-        hal.motion_sensor.run_motion_sensor()
-
-if __name__ == '__main__':
-    hal = Hal()
